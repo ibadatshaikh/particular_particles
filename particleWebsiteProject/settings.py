cab750b833c230eaf2ad9c86edf379c63850847e
ibadatshaikh <67431815+ibadatshaikh@users.noreply.github.com>
2023-08-10 19:59:57 +0200

    Update settings.py

--- particleWebsiteProject/settings.py
+++ particleWebsiteProject/settings.py
@@ -74,7 +74,12 @@ WSGI_APPLICATION = 'particleWebsiteProject.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
-
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
 database_url = os.environ.get("DATABASE_URL")
 DATABASES["default"] = dj_database_url.parse( database_url )
 
