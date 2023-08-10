f1d35c1a2bbed57511625a5e56e21f52fcff1dc7
ibadatshaikh <67431815+ibadatshaikh@users.noreply.github.com>
2023-08-10 22:59:55 +0200

    Moving Images

--- particleWebsiteApp/models.py
+++ particleWebsiteApp/models.py
@@ -8,7 +8,7 @@ class Particle( models.Model ):
 	summary = models.CharField( max_length = 500 )
 	short_summary = models.CharField( max_length = 200 )
 	fun_fact = models.CharField( max_length = 200 )
-	image = models.ImageField( upload_to = 'images/' )
+	image = models.CharField( max_length = 200 )
 	image_caption = models.CharField( max_length = 100 )
 	image_source = models.CharField( max_length = 500 )
 	def __str__( self ):
